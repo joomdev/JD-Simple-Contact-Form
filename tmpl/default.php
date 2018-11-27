efc9ea29898518abc2fb769755557617f1f41a0f
Hitesh Aggarwal <hitesh99raj@gmail.com>
2018-11-27 10:21:25 +0530

    Recaptcha Fixed

--- tmpl/default.php
+++ tmpl/default.php
@@ -43,15 +43,15 @@ if (!empty($message)) {
             $dispatcher = JEventDispatcher::getInstance();
             $dispatcher->trigger('onInit', 'jdscf_recaptcha_' . $module->id);
             $plugin = JPluginHelper::getPlugin('captcha', 'recaptcha');
-            $attributes = [];
-            $attributes['data-theme'] = $plugin_params->get('theme2', '');
-            $attributes['data-size'] = $plugin_params->get('size', '');
-            $attributeArray = [];
-            foreach ($attributes as $attributeKey => $attributeValue) {
-               $attributeArray[] = $attributeKey . '="' . $attributeValue . '"';
-            }
             if (!empty($plugin)) {
                $plugin_params = new JRegistry($plugin->params);
+               $attributes = [];
+               $attributes['data-theme'] = $plugin_params->get('theme2', '');
+               $attributes['data-size'] = $plugin_params->get('size', '');
+               $attributeArray = [];
+               foreach ($attributes as $attributeKey => $attributeValue) {
+                  $attributeArray[] = $attributeKey . '="' . $attributeValue . '"';
+               }
                ?>
                <div class="jdscf-row">
                   <div class="jdscf-col">
