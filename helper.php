ed872e90f88044dcfd737af536390b8c3515a6da
actuallyakash <akash_gupta@hotmail.com>
2019-02-08 13:40:44 +0530

    Textarea will retain <br> tags

--- helper.php
+++ helper.php
@@ -105,6 +105,12 @@ class ModJDSimpleContactFormHelper {
             $value = empty($value) ? 'unchecked' : 'checked';
          }
 
+         if ($fld['type'] == 'textarea') {
+            if ($value) {
+               $value = nl2br($value);
+            }
+         }
+
          $contents[] = [
              "value" => $value,
              "label" => $fld['label'],
